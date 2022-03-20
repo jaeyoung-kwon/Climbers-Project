9f78c00fdb522f6131d807041a548c2408fee332
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2022-03-20 13:26:36 +0900

    jaeyoung
    push push baby

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>update</Text>
+      <Text>KwonJaeYoung</Text>
       <StatusBar style="auto" />
     </View>
   );
